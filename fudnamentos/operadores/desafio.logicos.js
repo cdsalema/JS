00c9c64d205dacf894397ff60bf0ebcda5246980
cdsalema <conradod.salema@gmail.com>
2023-02-03 19:27:49 -0300

    desfio_logico

---
+++ fudnamentos/operadores/desafio.logicos.js
@@ -0,0 +1,19 @@
+//Duas expressões resultarem em TRUE(aritimeticos,relacionais,Logicos)
+let a=true;
+let b=true;
+
+let verdadeira1= a==b && 5+5>=10;
+let verdadeira2= a==b || 4-3>=1;
+
+console.log(verdadeira1);
+console.log(verdadeira2);
+
+//Duas expressões resultarem em False(aritimeticos,relacionais,Logicos)
+let c=true;
+let d=false;
+
+let Falsa1=c!=d && verdadeira1==false;
+let Falsa2=c==d && !verdadeira2;
+
+console.log(Falsa1);
+console.log(Falsa2);
