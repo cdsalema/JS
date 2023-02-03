42ec693f4df4ceba6ce31229306b20eea94e70d5
cdsalema <conradod.salema@gmail.com>
2023-02-03 13:50:06 -0300

    algoritimos basicos (conhecendo JS)

--- fudnamentos/inicio/dados.js
+++ fudnamentos/inicio/dados.js
@@ -1,7 +1,7 @@
 let nome="caneta";
 let quantidade =10;
 let preco = 6.4;
-const imposto = 1.5;
+const IMPOSTO = 1.5;
 let precoFinal= preco+imposto;
 let desconto = 0.4;
 let precoComDesconto= precoFinal*(1- desconto);
@@ -11,4 +11,4 @@ console.log("estoque: "+quantidade);
 console.log("valor unit.: "+ preco);
 console.log("IOF: " + imposto);
 console.log("Preço final: " +precoFinal);
-console.log("Promoção: " +precoComDesconto);
+console.log("Produto em promoção: " +precoComDesconto);
