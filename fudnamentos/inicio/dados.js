55179d4c74f0c9360a21b696b19d98158adc6f46
cdsalema <conradod.salema@gmail.com>
2023-02-03 13:08:53 -0300

    testando variaveis e constantes

--- fudnamentos/inicio/dados.js
+++ fudnamentos/inicio/dados.js
@@ -1,11 +1,14 @@
-let nome="caneta"
+let nome="caneta";
 let quantidade =10;
 let preco = 6.4;
-let imposto =1.5
+const imposto = 1.5;
 let precoFinal= preco+imposto;
+let desconto = 0.4;
+let precoComDesconto= precoFinal*(1- desconto);
 
 console.log("produto: "+ nome);
 console.log("estoque: "+quantidade);
 console.log("valor unit.: "+ preco);
 console.log("IOF: " + imposto);
 console.log("Preço final: " +precoFinal);
+console.log("Promoção: " +precoComDesconto);
