1424baff6e83af72823094336adb5127245e33c7
cdsalema <conradod.salema@gmail.com>
2023-02-03 13:00:11 -0300

    criando variaveis

---
+++ fudnamentos/inicio/dados.js
@@ -0,0 +1,11 @@
+let nome="caneta"
+let quantidade =10;
+let preco = 6.4;
+let imposto =1.5
+let precoFinal= preco+imposto;
+
+console.log("produto: "+ nome);
+console.log("estoque: "+quantidade);
+console.log("valor unit.: "+ preco);
+console.log("IOF: " + imposto);
+console.log("Preço final: " +precoFinal);
