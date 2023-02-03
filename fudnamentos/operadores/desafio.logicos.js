6ebc485a9c20f4252f86670ed26b62f8451fc580
cdsalema <conradod.salema@gmail.com>
2023-02-03 19:32:24 -0300

    desafio logico melhoria

--- fudnamentos/operadores/desafio.logicos.js
+++ fudnamentos/operadores/desafio.logicos.js
@@ -12,8 +12,8 @@ console.log(verdadeira2);
 let c=true;
 let d=false;
 
-let Falsa1=c!=d && verdadeira1==false;
-let Falsa2=c==d && !verdadeira2;
+let Falsa1=11+11==23 && c!=d && verdadeira1==false;
+let Falsa2=5+6!=11 && c==d || !verdadeira2;
 
 console.log(Falsa1);
 console.log(Falsa2);
