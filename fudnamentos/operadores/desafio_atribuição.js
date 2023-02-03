336164e6e2733f8049f9ca82bc5308151046f396
cdsalema <conradod.salema@gmail.com>
2023-02-03 18:26:12 -0300

    \n e \t

---
+++ fudnamentos/operadores/desafio_atribuição.js
@@ -0,0 +1,26 @@
+/*let texto= "Bom dia!\nTudo bem?";// \n quebra linha
+console.log(texto);
+
+let texto1= "Bom dia!\tTudo bem?";// \t da um tab
+console.log(texto1);
+
+--DESAFIO - imprima a lista abaixo 
+
+Lista de Aprovados
+==================
+
+1.Ana Silva
+2.Pedro Albuquerque
+3.Guilherme Pereira
+4.Rebeca França
+
+*/
+
+let texto2="Lista de Aprovados\n";
+texto2+="==================\n\n";
+texto2+="1.Ana Silva\n";
+texto2+="2.Pedro Albuquerque\n";
+texto2+="3.Guilherme Pereira\n";
+texto2+="4.Rebeca França";
+
+console.log(texto2);
