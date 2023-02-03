5b115eeea6f7c0bc78397edb3ece550daa52408f
cdsalema <conradod.salema@gmail.com>
2023-02-03 19:00:32 -0300

    operadores logicos

---
+++ fudnamentos/operadores/logicos.js
@@ -0,0 +1,11 @@
+const t1=false;
+const t2= false;
+
+let comprarTv50=t1&&t2;
+console.log("Vou comprar tv de 50pol?",comprarTv50);
+let comprarTv32=t1!==t2;
+console.log("Vou comprar tv de 32pol?",comprarTv32);
+let tomarSorvete=t1||t2;
+console.log("Vou tomar sorvete?",tomarSorvete);
+let ficaEmCasa= !tomarSorvete;
+console.log("Vou ficar em casa?",ficaEmCasa);
