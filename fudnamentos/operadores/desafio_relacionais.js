07d3306696c01bc9e0e9d2849cfbe71605ea6c92
cdsalema <conradod.salema@gmail.com>
2023-02-03 15:24:47 -0300

    relacionais e arimeticos

---
+++ fudnamentos/operadores/desafio_relacionais.js
@@ -0,0 +1,16 @@
+let a=1;
+let b=2;
+
+//5 resultados true
+console.log(a<b);
+console.log(a!==b);
+console.log(a<=b);
+console.log(a!=b);
+console.log(a+a===b);
+
+//5 resultados false
+console.log(a>b);
+console.log(a+a>b);
+console.log(a==b);
+console.log(a===b);
+console.log(a>=b);
