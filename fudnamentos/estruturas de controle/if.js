19d477ac4e8cca68c99eb33ce61f87e894871201
cdsalema <conradod.salema@gmail.com>
2023-02-03 20:55:14 -0300

    estruturas de comtrole if

---
+++ fudnamentos/estruturas de controle/if.js
@@ -0,0 +1,8 @@
+let nota = 6;
+const mensagem = 'Boas férias!'
+if (nota >= 6){
+    console.log("Aprovado, " + mensagem);
+}
+if (nota <= 5.9){
+    console.log("Reprovado, " + mensagem + " E estude mais!");
+}
---
+++ fudnamentos/operadores/ternario.js
@@ -0,0 +1,8 @@
+const hora=14;
+const saudacao=hora<=11 ?"Bom dia!":"Boa tarde!";
+console.log(saudacao);
+
+//parte2
+const hora2=11;
+const saudacao2= hora2 <= 11 ? "Bom dia!" : hora2<=17?"Boa tarde!":"Boa noite!";
+console.log(saudacao2);
