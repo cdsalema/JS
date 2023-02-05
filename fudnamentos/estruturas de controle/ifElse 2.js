1c8e86b7a3d80d4eba41cd35171d035519fa08e5
cdsalema <conradod.salema@gmail.com>
2023-02-05 17:48:23 -0300

    code cleaning

--- fudnamentos/estruturas de controle/ifElse 2.js
+++ fudnamentos/estruturas de controle/ifElse 2.js
@@ -1,14 +1,13 @@
-const hora = 5;
+const hora = 18;
 let saudacao;
 
 if (hora >= 6 && hora < 12) {
     saudacao = "Bom dia!"
-} else {
+} else
     if (hora >= 12 && hora < 18) {
         saudacao = "Boa tarde!"
     }
     else //(hora >= 18 && hora < 6)//
         saudacao = "Boa Noite!"
 
-}
 console.log(saudacao);
