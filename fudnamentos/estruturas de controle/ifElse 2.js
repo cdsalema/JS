769d7d9d8227f53343ddd822c969f8fed2ed900b
cdsalema <conradod.salema@gmail.com>
2023-02-05 17:30:15 -0300

    IF_ELSE exercise3

--- fudnamentos/estruturas de controle/ifElse 2.js
+++ fudnamentos/estruturas de controle/ifElse 2.js
@@ -3,16 +3,12 @@ let saudacao;
 
 if (hora >= 6 && hora < 12) {
     saudacao = "Bom dia!"
-    console.log(saudacao);
 } else {
-    if (hora < 18) {
+    if (hora >= 12 && hora < 18) {
         saudacao = "Boa tarde!"
-        console.log(saudacao);
     }
-    else (hora >= 18 && hora < 6)
-    saudacao = "Boa Noite!"
-    console.log(saudacao);
-
+    else //(hora >= 18 && hora < 6)//
+        saudacao = "Boa Noite!"
 
 }
-
+console.log(saudacao);
