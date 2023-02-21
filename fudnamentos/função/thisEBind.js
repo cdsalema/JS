e8d0853c8466b3bc1d7f4859e8ae6ce694e8872a
cdsalema <conradod.salema@gmail.com>
2023-02-20 23:50:48 -0300

    docs- estudo JS

--- fudnamentos/função/thisEBind.js
+++ fudnamentos/função/thisEBind.js
@@ -6,3 +6,8 @@ const pessoa={
 }
 
 pessoa.falar()
+const falar = pessoa.falar
+falar() // conflito entre paradigmas: funcionla e OO.
+
+const falarDePessoa = pessoa.falar.bind(pessoa)
+falarDePessoa()
