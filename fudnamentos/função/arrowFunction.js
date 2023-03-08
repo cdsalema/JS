53ad00ebbcd82300d4e697184d217d2040c3acda
cdsalema <conradod.salema@gmail.com>
2023-03-08 16:54:15 -0300

    docs - estudo de Js

---
+++ fudnamentos/função/arrowFunction.js
@@ -0,0 +1,10 @@
+//function padrao literal
+let dorbro = function (a) {
+    return 2 * a
+}
+// mesma function porém em formato ARROW
+dobro = (a) => {
+    return 2 * a
+}
+//Arrow function mas com somente 1 parametro e somente 1 linha
+dobro = a => 2 * a //o return não é necessário pois é implicito.
