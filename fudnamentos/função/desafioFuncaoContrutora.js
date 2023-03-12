b739feba2c8c61f3122e855ca9bfad0278fc6856
cdsalema <conradod.salema@gmail.com>
2023-03-12 02:37:58 -0300

    docs - mais sobre js

---
+++ fudnamentos/função/desafioFuncaoContrutora.js
@@ -0,0 +1,8 @@
+function CriarPessoa(nome) {
+    this.nome = nome
+    this.falar = function () {
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+const p1 = new CriarPessoa("Conrado")
+p1.falar()
---
+++ fudnamentos/função/factory.js
@@ -0,0 +1,8 @@
+function Criarprod1(nome, preço, desconto) {
+    return {
+        nome,
+        preço,
+        desconto,
+    }
+}
+console.log(Criarprod1("Arroz", 25, 0.1))
